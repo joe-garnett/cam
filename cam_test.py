bb40522c02b07cc73df1e849b51e86ef95bf424a
Joseph Garnett <jgarnett370@gmail.com>
2024-01-27 17:47:02 +0000

    add file management

--- cam_test.py
+++ cam_test.py
@@ -1,8 +1,25 @@
 from picamera2 import Picamera2, Preview
+from . import file_manager
 import time
+from os import path
+
+
+
 picam2 = Picamera2()
 camera_config = picam2.create_preview_configuration()
 picam2.configure(camera_config)
 picam2.start()
-time.sleep(2)
-picam2.capture_file("test.jpg")
+
+exit_flag = False
+
+while not exit_flag: 
+    score = input("Enter combined score: ")
+    if score.lower() == "exit":
+        exit_flag = True
+    else:   
+        filename = score + '.jpg'
+        picam2.capture_file(path.join(".", "unsorted_image", filename))
+        file_manager.add_image(filename)
+
+picam2.stop()
+picam2.close()
---
+++ file_manager.py
@@ -0,0 +1,62 @@
+import os
+from PIL import Image
+
+# dir paths
+base_path = os.path.join(".", "data")
+unsorted_path = os.path.join(".", "unsorted_images")
+
+def create_master_files():
+    # Loop from 0 to 180
+    for i in range(181):
+        # Folder name as the current number
+        folder_name = str(i)
+        # Full path for the new folder
+        new_path = os.path.join(base_path, folder_name)
+        
+        # Create the folder if it doesn't exist
+        if not os.path.exists(new_path):
+            os.makedirs(new_path)
+            print(f"Folder '{folder_name}' created.")
+        else:
+            print(f"Folder '{folder_name}' already exists.")
+
+def add_image(score_img):
+    # Pass filename of file in unsorted_images
+
+    # Splits filename into score and suffix e.g. "64" "jpg"
+    score, suffix = score_img.split(".")
+    dir = os.path.join(base_path, str(score))
+    dir_contents = os.listdir(dir)
+
+    # Creates lowest possible unique number id for photo, just to keep things nice and organised
+    ids = set()
+    for f in dir_contents:
+        index, _= f.split(".")
+        ids.add(int(index))
+    if ids:
+        maximum = max(ids)
+    else: 
+        maximum = -1
+    minimum = 0
+
+    possible_ids = set()
+    for i in range(minimum, maximum + 2):
+        possible_ids.add(i)
+
+    available_ids = possible_ids - ids
+    id = min(available_ids)
+    save_image(score_img, (str(id) + '.' + suffix), dir)
+
+    
+
+
+def save_image(score_img, filename, target_dir):
+    old_file_path = os.path.join(unsorted_path, score_img)
+    image = Image.open(old_file_path)
+    # Save the image to the correct directory with a new filename
+    image.save(os.path.join(target_dir, filename))
+    # Remove old file
+    os.remove(old_file_path)
+
+
+    
