03fb7a829d820f28a9d957a50abfacfa3b3347c6
Joseph Garnett <jgarnett370@gmail.com>
2024-01-27 19:26:52 +0000

    better quality

--- cam_test.py
+++ cam_test.py
@@ -6,6 +6,8 @@ import os
 
 
 picam2 = Picamera2()
+picam2.options["quality"] = 95
+picam2.options["compress_level"] = 0
 camera_config = picam2.create_preview_configuration()
 picam2.configure(camera_config)
 picam2.start()
